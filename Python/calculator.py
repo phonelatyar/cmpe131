d766342d2635d0ffbc76f11df9ee67a3729f8134
Phone Latyar <phone.latyar@sjsu.edu>
2021-03-05 23:09:25 -0800

    This is the third commit for both investment.py and calculator.py
    Just add the comment for both source files
    Also add the function to output false if the user put invaild value in the investment.py

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,6 +1,9 @@
 import math
 
 def calculator (number1, number2, operator):
+	''' Check the input operator from the user and 
+		match with the correspond function operator
+	'''	
 		if operator == "+":	
 			result = number1 + number2
 		elif operator == "-":	
@@ -13,15 +16,16 @@ def calculator (number1, number2, operator):
 			result = number1 // number2
 		elif operator == "**":	
 			result = number1 ** number2
-		print(str(result)+"\n")
+		print(str(result)+"\n") # change the result into string to add new line at the end
 		
 def menu():
+	''' Load the prompt to the user display and call the calculator function'''
 	number1 = float(input("Enter the first number: "))
 	number2 = float(input("Enter the second number : "))
 	operator = input("Enter the operation: ")
 	calculator (number1, number2, operator)
 
-while True:
+while True:	# to break or continue the calculator function
 	menu()
 	exit = (str(input("Do you wish to exit? ")))
 	if exit == "y":
